c5c3bcc56063d07b48715ca20bd03760696cc6bf
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2022-10-31 13:33:58 +0000

    set up node

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const path = require('path');
+const public = path.join(__dirname,'public');
+app.use(express.static(public));
+app.get('/', function(req, res) {
+    res.send('Hello! Welcome to my application.');
+    })
+    
+app.get('/home', function(req, res) {
+res.sendFile(path.join(public, 'index.html'));
+})
+
+app.use(function(req, res) {
+res.status(404);
+res.send('Oops! We didn\'t find what you are looking for.');
+})
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
