26a6f0b221fa421a2c4ce99c94e10bbcf4a744e2
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2022-10-31 13:56:18 +0000

    procfile added

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-const path = require('path');
-const public = path.join(__dirname,'public');
+const path = require("path");
+const public = path.join(__dirname, "public");
 app.use(express.static(public));
-app.get('/', function(req, res) {
-    res.sendFile(path.join(public, 'index.html'));
-    })
-    
-app.get('/home', function(req, res) {
-res.sendFile(path.join(public, 'index.html'));
-})
+app.get("/", function (req, res) {
+  res.sendFile(path.join(public, "index.html"));
+});
 
-app.use(function(req, res) {
-res.status(404);
-res.send('Oops! We didn\'t find what you are looking for.');
-})
+app.use(function (req, res) {
+  res.status(404);
+  res.send("Oops! We didn't find what you are looking for.");
+});
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-  });
+  console.log(`Server listening on ${PORT}`);
+});
